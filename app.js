d234dfe09d802385e9d75b5688404668e6b13162
satywan kumar <satywankumar646@gmail.com>
2024-09-01 15:54:34 +0530

    demo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+app.use(bodyParser.json());
+
+app.post('/getResponse', (req, res) => {
+    console.log(req.body.question)
+    const genAI = new GoogleGenerativeAI('AIzaSyBMDJmXsX4Yr7xRGsjnFowk3m0yvcaTxaU');
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    model.generateContent(req.body.question).then(result=>{
+        console.log(result.response.text());
+        const response = result.response.text();
+        res.status(200).json({
+            response:response
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
+    })
+})
+
+
+
+module.exports = app;
